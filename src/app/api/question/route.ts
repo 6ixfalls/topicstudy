b3b81771a1ea9c4e3df7846e5295fede13236b90
Six <23470032+6ixfalls@users.noreply.github.com>
2023-09-29 21:54:22 -0700

    Question debug

--- src/app/api/question/route.ts
+++ src/app/api/question/route.ts
@@ -1,14 +1,55 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import openai from "@/lib/openai";
 import OpenAI from "openai";
 
-export async function POST() {
+export async function POST(request: NextRequest) {
+    const body = await request.json();
     const params: OpenAI.Chat.ChatCompletionCreateParams = {
-        messages: [{ role: "system", content: "Hello, how are you?" }],
-        model: "gpt-3.5-turbo",
+        messages: body.context.concat([
+            {
+                role: "system",
+                content:
+                    "Generate a multiple choice question about the above paragraph you wrote about. The question should be easy to answer based on the paragraph you wrote, and should be easy to understand. The question should have 4 possible answers. The question should also be related to the topic provided by the user. Do not repeat any questions you already asked, and keep a diverse set of questions and answers.",
+            },
+        ]),
+        model: "gpt-3.5-turbo-0613",
+        functions: [
+            {
+                name: "generate_question",
+                description:
+                    "Add a question to the context with the question and 4 possible answers.",
+                parameters: {
+                    type: "object",
+                    properties: {
+                        question: { type: "string" },
+                        answers: {
+                            type: "array",
+                            items: {
+                                type: "object",
+                                properties: {
+                                    answer: { type: "string" },
+                                    correct: { type: "boolean" },
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        ],
     };
     const completion = await openai.chat.completions.create(params);
+    if (!completion.choices[0].message.function_call) {
+        return new NextResponse("Internal Server Error", { status: 400 });
+    }
+    const question = await JSON.parse(
+        completion.choices[0].message.function_call.arguments
+    );
     return NextResponse.json({
-        paragraph: completion.choices[0].message.content,
+        messages: params.messages.push({
+            role: "assistant",
+            content: question.question,
+        }),
+        question: question.question,
+        answers: question.answers,
     });
 }
--- src/components/pages/question.tsx
+++ src/components/pages/question.tsx
@@ -1,3 +1,50 @@
-export default function Question() {
-    return <div>Question</div>;
+import OpenAI from "openai";
+import { useState } from "react";
+import { useEffectOnce } from "./paragraph";
+import * as z from "zod";
+import { formSchema } from "./topic";
+
+export default function Question({
+    topic,
+    context,
+    setPage,
+    setContext,
+}: {
+    topic: z.infer<typeof formSchema>;
+    setTopic: (topic: z.infer<typeof formSchema>) => void;
+    setPage: (page: string) => void;
+    context: OpenAI.Chat.ChatCompletionCreateParams["messages"];
+    setContext: (
+        context: OpenAI.Chat.ChatCompletionCreateParams["messages"]
+    ) => void;
+}) {
+    const [generating, setGenerating] = useState<boolean>(true);
+    const [question, setQuestion] = useState<string>("");
+    useEffectOnce(() => {
+        fetch("/api/question", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ context }),
+        }).then(async (res) => {
+            if (res.ok) {
+                const data = await res.json();
+                setQuestion(data.question);
+                setContext(data.messages);
+                setGenerating(false);
+            }
+        });
+    });
+    return (
+        <div className="container">
+            <h1 className="text-xl font-semibold">Answer the question</h1>
+            <span className="flex flex-row items-center gap-1 mb-2">
+                Answer the question below based on the passage you just read.
+            </span>
+            {generating ? (
+                <span className="animate-pulse">Loading Question</span>
+            ) : (
+                <article>{question}</article>
+            )}
+        </div>
+    );
 }
