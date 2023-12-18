4b925571a89fbfe16d323222c33c307ea35e3f87
Six <23470032+6ixfalls@users.noreply.github.com>
2023-12-17 17:03:02 -0800

    Cloudflare AI Gateway

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -2,7 +2,7 @@ import OpenAI from "openai";
 
 const openai = new OpenAI({
     apiKey: process.env.OAI_API_KEY,
-    //baseURL: process.env.OAI_BASE_PATH,
+    baseURL: process.env.OAI_BASE_PATH,
     organization: process.env.OAI_ORGANIZATION,
 });
 
